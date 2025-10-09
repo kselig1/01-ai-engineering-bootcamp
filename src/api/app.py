ec1f72370ac287d4e6607dddcd559994705398a3
kselig1 <you@example.com>
2025-10-08 22:00:26 -0700

    moved the RAG pipeline to the backend app and updated the frontend for those changes

--- src/api/app.py
+++ src/api/app.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, Request 
-from pydantic import BaseModel 
-
-from openai import OpenAI
-from groq import Groq
-from google import genai
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.core.config import config
+from src.api.api.endpoints import api_router
 
 import logging 
+from src.api.api.middleware import RequestIdMiddleware
 
 logging.basicConfig( 
     level = logging.INFO, 
@@ -15,45 +13,21 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def run_llm(provider, model_name, messages, max_tokens=500, temperature=0.0):
-
-    if provider == "OpenAI":
-        client = OpenAI(api_key=config.OPENAI_API_KEY)
-    elif provider == "Groq":
-        client = Groq(api_key=config.GROQ_API_KEY)
-    else:
-        client = genai.Client(api_key=config.GOOGLE_API_KEY)
-
-    if provider == "Google":
-        return client.models.generate_content(
-            model = model_name,
-            contents=[message["content"] for message in messages],
-            generation_config={"temperature": temperature}
-        ).text
-    else:
-        return client.chat.completions.create(
-            model = model_name,
-            messages=messages,
-            max_tokens=max_tokens,
-            temperature=temperature
-        ).choices[0].message.content
-
-class ChatRequest(BaseModel):
-    provider: str
-    model_name: str
-    messages: list[dict]
-    temperature: float = 0.0
-
-class ChatResponse(BaseModel):
-    message: str
-
 app = FastAPI()
-@app.post("/chat")
-def chat(
-    request: Request,
-    payload: ChatRequest
-    ) -> ChatResponse:
-    
-    result = run_llm(payload.provider, payload.model_name, payload.messages, temperature=payload.temperature)
-    
-    return ChatResponse(message=result)
+
+app.add_middleware(RequestIdMiddleware) 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router)
+
+@app.get("/")
+async def root(): 
+    """Root endpoint that returns a wecome message""" 
+    return {"message": "API"} 
--- src/chatbot_ui/app.py
+++ src/chatbot_ui/app.py
@@ -3,35 +3,6 @@ import requests
 
 from core.config import config
 
-# Create a sidebar with a dropdown for the model list and providers
-with st.sidebar:
-    st.title("Settings")
-
-    # Dropdown for model provider
-    provider = st.selectbox("Provider", ["OpenAI", "Groq", "Google"])
-
-    if provider == "OpenAI":
-        model_name = st.selectbox("Model", ["gpt-4o-mini", "gpt-4o"])
-    elif provider == "Groq":
-        model_name = st.selectbox("Model", ["llama-3.3-70b-versatile"])
-    else:
-        model_name = st.selectbox("Model", ["gemini-2.0-flash"])
-
-    # Temperature slider for controlling randomness
-    temperature = st.slider(
-        "Temperature", 
-        min_value=0.0, 
-        max_value=2.0, 
-        value=0.0, 
-        step=1.0,
-        help="Controls randomness in the response. Lower values make responses more focused while higher values make them random."
-    )
-
-    # Save provider and model to session state, temperature
-    st.session_state.provider = provider
-    st.session_state.model_name = model_name
-    st.session_state.temperature = temperature
-
 def api_call(method, url, **kwargs):
 
     def _show_error_popup(message):
@@ -77,12 +48,8 @@ if prompt := st.chat_input("Hello! How can I assist you today?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        output = api_call("post", f"{config.API_URL}/chat", json={
-            "provider": st.session_state.provider, 
-            "model_name": st.session_state.model_name, 
-            "messages": st.session_state.messages,
-            "temperature": st.session_state.temperature})
+        output = api_call("post", f"{config.API_URL}/rag", json={"query": prompt})
         response_data = output[1]
-        answer = response_data["message"]
+        answer = response_data["answer"]
         st.write(answer)
     st.session_state.messages.append({"role": "assistant", "content": answer})
