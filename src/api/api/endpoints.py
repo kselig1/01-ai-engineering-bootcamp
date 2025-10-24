2eaa603f576951d694529b6abb818dd1d3204c61
kselig1 <you@example.com>
2025-10-24 15:09:50 -0700

    moved ReAct agent implementation to our backend

--- src/api/api/endpoints.py
+++ src/api/api/endpoints.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, Request
 import logging 
 
-from api.api.models import RAGRequest, RAGResponse, RAGUsedContext
-
-from api.rag.retrieval_generation import rag_pipeline_wrapper
+from api.api.models import AgentRequest, AgentResponse, RAGUsedContext
+from api.agent.graph import run_agent_wrapper
 
 logger = logging.getLogger(__name__) 
 
@@ -12,10 +11,10 @@ rag_router = APIRouter()
 @rag_router.post("/") 
 def rag( 
     request: Request, 
-    payload: RAGRequest
-) -> RAGResponse: 
-    answer = rag_pipeline_wrapper(payload.query) 
-    return RAGResponse(
+    payload: AgentRequest
+) -> AgentResponse: 
+    answer = run_agent_wrapper(payload.query) 
+    return AgentResponse(
         request_id=request.state.request_id,
         answer=answer["answer"],
         used_context=[RAGUsedContext(**used_context) for used_context in answer["used_context"]])
